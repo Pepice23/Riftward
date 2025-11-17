d3f902f90365731a2f146fb8064c8210e4c14bde
Molnár József <mojo12984@gmail.com>
2025-11-17 19:38:57 +0100

    moved levelupUI, projectile and hud into the player folder

---
+++ Player/LevelUpUi.cs
@@ -0,0 +1,48 @@
+using Godot;
+
+public partial class LevelUpUi : CanvasLayer
+{
+    [Export] public Player Player;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        Hide();
+        Player.GamePaused += ShowUpgradeUi;
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+    }
+
+    private void ShowUpgradeUi()
+    {
+        GetTree().Paused = true;
+        Show();
+    }
+
+    private void HideUpgradeUi()
+    {
+        Hide();
+        GetTree().Paused = false;
+    }
+
+    private void OnUpgrade1Selected()
+    {
+        GD.Print("Player chose Upgrade 1");
+        HideUpgradeUi();
+    }
+
+    private void OnUpgrade2Selected()
+    {
+        GD.Print("Player chose Upgrade 2");
+        HideUpgradeUi();
+    }
+
+    private void OnUpgrade3Selected()
+    {
+        GD.Print("Player chose Upgrade 3");
+        HideUpgradeUi();
+    }
+}

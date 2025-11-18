5e23520a1cc8e01b7309e39a4173aabfb0f7271c
Molnár József <mojo12984@gmail.com>
2025-11-18 09:38:11 +0100

    Now serving random upgrades

--- Player/LevelUpUi.cs
+++ Player/LevelUpUi.cs
@@ -1,8 +1,19 @@
+using System.Collections.Generic;
+using System.Linq;
 using Godot;
 
 public partial class LevelUpUi : CanvasLayer
 {
     [Export] public Player Player;
+
+    private UpgradeButton _upgradeButton1;
+    private UpgradeButton _upgradeButton2;
+    private UpgradeButton _upgradeButton3;
+
+    // Store the current 3 upgrades
+    private Upgrade _currentUpgrade1;
+    private Upgrade _currentUpgrade2;
+    private Upgrade _currentUpgrade3;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
@@ -12,6 +23,10 @@ public partial class LevelUpUi : CanvasLayer
         {
             Player.GamePaused += ShowUpgradeUi;
         }
+
+        _upgradeButton1 = GetNode<UpgradeButton>("%UpgradeButton1");
+        _upgradeButton2 = GetNode<UpgradeButton>("%UpgradeButton2");
+        _upgradeButton3 = GetNode<UpgradeButton>("%UpgradeButton3");
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -21,6 +36,15 @@ public partial class LevelUpUi : CanvasLayer
 
     private void ShowUpgradeUi()
     {
+        var randomUpgrades = GetRandomUpgrades();
+
+        _currentUpgrade1 = randomUpgrades[0];
+        _currentUpgrade2 = randomUpgrades[1];
+        _currentUpgrade3 = randomUpgrades[2];
+
+        _upgradeButton1.SetUpgradeNameAndDescription(_currentUpgrade1.Name, _currentUpgrade1.Description);
+        _upgradeButton2.SetUpgradeNameAndDescription(_currentUpgrade2.Name, _currentUpgrade2.Description);
+        _upgradeButton3.SetUpgradeNameAndDescription(_currentUpgrade3.Name, _currentUpgrade3.Description);
         GetTree().Paused = true;
         Show();
     }
@@ -33,19 +57,19 @@ public partial class LevelUpUi : CanvasLayer
 
     private void OnUpgrade1Selected()
     {
-        GD.Print("Player chose Upgrade 1");
+        _currentUpgrade1.ApplyEffect(Player);
         HideUpgradeUi();
     }
 
     private void OnUpgrade2Selected()
     {
-        GD.Print("Player chose Upgrade 2");
+        _currentUpgrade2.ApplyEffect(Player);
         HideUpgradeUi();
     }
 
     private void OnUpgrade3Selected()
     {
-        GD.Print("Player chose Upgrade 3");
+        _currentUpgrade3.ApplyEffect(Player);
         HideUpgradeUi();
     }
 
@@ -55,5 +79,15 @@ public partial class LevelUpUi : CanvasLayer
         {
             Player.GamePaused -= ShowUpgradeUi;
         }
+    }
+
+    private List<Upgrade> GetRandomUpgrades()
+    {
+        var randomUpgrades = UpgradeManager.Instance.AllUpgrades
+            .OrderBy(x => GD.Randf()) // Shuffle randomly
+            .Take(3) // Take first 3
+            .ToList(); // Convert to List
+
+        return randomUpgrades;
     }
 }

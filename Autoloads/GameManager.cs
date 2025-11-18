f30616059dbf52e023d4503766d477d9bc36d5de
Molnár József <mojo12984@gmail.com>
2025-11-18 10:55:09 +0100

    Update GameManager.cs

--- Autoloads/GameManager.cs
+++ Autoloads/GameManager.cs
@@ -1,6 +1,13 @@
 using Godot;
-using System;
 
 public partial class GameManager : Node
 {
+    public static GameManager Instance { get; private set; }
+    public bool IsRunActive;
+    public float RunTime;
+
+    public override void _Ready()
+    {
+        Instance = this;
+    }
 }

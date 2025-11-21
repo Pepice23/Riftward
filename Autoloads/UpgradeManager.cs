95b9b914672f9a9b9278798edc038ef1e028e985
Molnár József <mojo12984@gmail.com>
2025-11-21 12:51:53 +0100

    Added class specific upgrades

--- Autoloads/UpgradeManager.cs
+++ Autoloads/UpgradeManager.cs
@@ -8,17 +8,18 @@ public partial class UpgradeManager : Node
 {
     public static UpgradeManager Instance { get; private set; }
     public readonly List<Upgrade> AllUpgrades = [];
+    private readonly List<Upgrade> _paladinUpgrades = [];
+    private readonly List<Upgrade> _mageUpgrades = [];
+    private readonly List<Upgrade> _hunterUpgrades = [];
+
 
     public override void _Ready()
     {
         Instance = this;
-        InitializeUpgrades();
     }
 
-    private void InitializeUpgrades()
+    private void AllUpgradeList()
     {
-        // Create upgrade instances and add them to the list
-
         AllUpgrades.Add(new Upgrade
         {
             Name = "Damage Boost",
@@ -42,20 +43,40 @@ public partial class UpgradeManager : Node
                 GD.Print($"After upgrade: Movement Speed = {player.Speed}");
             }
         });
+    }
 
-        AllUpgrades.Add(new Upgrade
+    private void PaladinUpgradeList()
+    {
+        _paladinUpgrades.Add(new Upgrade
+            {
+                Name = "Increased Aura Radius",
+                Description = "+25 Increased Aura Radius",
+                ApplyEffect = player =>
+                {
+                    GD.Print($"Before upgrade: Aura radius = {player.AuraRadius}");
+                    player.AuraRadius += 25;
+                    player.UpdateHammerPositions(player.AuraRadius);
+                    GD.Print($"After upgrade: Aura Radius = {player.AuraRadius}");
+                }
+            }
+        );
+    }
+
+    private void MageUpgradeList()
+    {
+        _mageUpgrades.Add(new Upgrade
         {
-            Name = "Projectile Speed Boost",
-            Description = "+100 Speed to projectiles",
+            Name = "Bolt Speed Boost",
+            Description = "+100 Speed to Bolts",
             ApplyEffect = (player) =>
             {
-                GD.Print($"Before upgrade: Projectile Speed = {player.ProjectileSpeed}");
+                GD.Print($"Before upgrade: Bolt Speed = {player.ProjectileSpeed}");
                 player.ProjectileSpeed += 100;
-                GD.Print($"After upgrade: Projectile Speed = {player.ProjectileSpeed}");
+                GD.Print($"After upgrade: Bolt Speed = {player.ProjectileSpeed}");
             }
         });
 
-        AllUpgrades.Add(new Upgrade
+        _mageUpgrades.Add(new Upgrade
         {
             Name = "Faster Shooting",
             Description = "-0.05 to Attack Cooldown",
@@ -66,5 +87,64 @@ public partial class UpgradeManager : Node
                 GD.Print($"After upgrade: Attack Cooldown = {player.AttackCooldown}");
             }
         });
+    }
+
+    private void HunterUpgradeList()
+    {
+        _hunterUpgrades.Add(new Upgrade
+        {
+            Name = "Arrow Speed Boost",
+            Description = "+100 Speed to arrow",
+            ApplyEffect = (player) =>
+            {
+                GD.Print($"Before upgrade: Arrow Speed = {player.ProjectileSpeed}");
+                player.ProjectileSpeed += 100;
+                GD.Print($"After upgrade: Arrow Speed = {player.ProjectileSpeed}");
+            }
+        });
+
+        _hunterUpgrades.Add(new Upgrade
+        {
+            Name = "Faster Shooting",
+            Description = "-0.05 to Attack Cooldown",
+            ApplyEffect = (player) =>
+            {
+                GD.Print($"Before upgrade: Attack Cooldown = {player.AttackCooldown}");
+                player.AttackCooldown -= 0.05f;
+                GD.Print($"After upgrade: Attack Cooldown = {player.AttackCooldown}");
+            }
+        });
+    }
+
+    public void InitializeUpgrades()
+    {
+        GD.Print($"Selected class: {GameManager.Instance.SelectedClass}");
+        AllUpgradeList();
+        if (GameManager.Instance.SelectedClass == GameManager.PlayerClass.Paladin)
+        {
+            PaladinUpgradeList();
+            foreach (var upgrade in _paladinUpgrades)
+            {
+                AllUpgrades.Add(upgrade);
+            }
+        }
+
+        if (GameManager.Instance.SelectedClass == GameManager.PlayerClass.Mage)
+        {
+            MageUpgradeList();
+            foreach (var upgrade in _mageUpgrades)
+            {
+                AllUpgrades.Add(upgrade);
+            }
+        }
+
+        if (GameManager.Instance.SelectedClass == GameManager.PlayerClass.Hunter)
+        {
+            HunterUpgradeList();
+            foreach (var upgrade in _hunterUpgrades)
+            {
+                AllUpgrades.Add(upgrade);
+            }
+        }
     }
 }
--- UI/ClassSelection.cs
+++ UI/ClassSelection.cs
@@ -10,17 +10,20 @@ public partial class ClassSelection : HBoxContainer
     {
         GameManager.Instance.SelectedClass = GameManager.PlayerClass.Paladin;
         GetTree().ChangeSceneToFile("res://main.tscn");
+        UpgradeManager.Instance.InitializeUpgrades();
     }
 
     private void SelectMage()
     {
         GameManager.Instance.SelectedClass = GameManager.PlayerClass.Mage;
         GetTree().ChangeSceneToFile("res://main.tscn");
+        UpgradeManager.Instance.InitializeUpgrades();
     }
 
     private void SelectHunter()
     {
         GameManager.Instance.SelectedClass = GameManager.PlayerClass.Hunter;
         GetTree().ChangeSceneToFile("res://main.tscn");
+        UpgradeManager.Instance.InitializeUpgrades();
     }
 }

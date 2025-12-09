67501660efd2067d7245968a5766fbf5b5216898
Molnár József <mojo12984@gmail.com>
2025-12-09 09:38:56 +0100

    Win and Lose screens now show the gold earned during run

--- UI/WinScreen.cs
+++ UI/WinScreen.cs
@@ -1,10 +1,25 @@
 using Godot;
 using System;
 
+// ReSharper disable once CheckNamespace
+#pragma warning disable CA1050
 public partial class WinScreen : PanelContainer
+#pragma warning restore CA1050
 {
+	private Label _currentRunGold;
+	public override void _Ready()
+	{
+		_currentRunGold = GetNode<Label>("%RunGoldLabel");
+		UpdateRunGold(GameManager.Instance.RunGold);
+	}
 	private void GoBackToMainMenu()
 	{
 		GetTree().ChangeSceneToFile("res://UI/main_menu.tscn");
+		GameManager.Instance.ResetGame();
+	}
+	
+	private void UpdateRunGold(int runGold)
+	{
+		_currentRunGold.Text = runGold.ToString();
 	}
 }

24f31e6f59b9d7f8987576bdba52bc6f5a44fb43
Molnár József <mojo12984@gmail.com>
2025-12-11 16:04:06 +0100

    changed to go to hub instead of main menu

--- UI/WinScreen.cs
+++ UI/WinScreen.cs
@@ -6,20 +6,22 @@ using System;
 public partial class WinScreen : PanelContainer
 #pragma warning restore CA1050
 {
-	private Label _currentRunGold;
-	public override void _Ready()
-	{
-		_currentRunGold = GetNode<Label>("%RunGoldLabel");
-		UpdateRunGold(GameManager.Instance.RunGold);
-	}
-	private void GoBackToMainMenu()
-	{
-		GetTree().ChangeSceneToFile("res://UI/main_menu.tscn");
-		GameManager.Instance.ResetGame();
-	}
-	
-	private void UpdateRunGold(int runGold)
-	{
-		_currentRunGold.Text = runGold.ToString();
-	}
+    private Label _currentRunGold;
+
+    public override void _Ready()
+    {
+        _currentRunGold = GetNode<Label>("%RunGoldLabel");
+        UpdateRunGold(GameManager.Instance.RunGold);
+    }
+
+    private void GoBackToMainMenu()
+    {
+        GetTree().ChangeSceneToFile("res://UI/hub.tscn");
+        GameManager.Instance.ResetGame();
+    }
+
+    private void UpdateRunGold(int runGold)
+    {
+        _currentRunGold.Text = runGold.ToString();
+    }
 }

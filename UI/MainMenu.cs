6b53739cc4eea41bb050a8f90c84fccb52ff1a01
Molnár József <mojo12984@gmail.com>
2025-12-12 10:55:27 +0100

    Main menu goes to hub instead of character select

--- UI/MainMenu.cs
+++ UI/MainMenu.cs
@@ -1,31 +1,36 @@
 using Godot;
-using System;
 
+
+// ReSharper disable once CheckNamespace
+#pragma warning disable CA1050
 public partial class MainMenu : VBoxContainer
+#pragma warning restore CA1050
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+    }
 
-	private void OnWinterModeButtonPressed(bool isChecked)
-	{
-		GameManager.Instance.IsWinterModeEnabled = isChecked;
-		if (GameManager.Instance.IsWinterModeEnabled)
-		{
-			GD.Print("Winter mode is enabled");
-		}
-		else{
-			GD.Print("Winter mode is disabled");
-		}
-	}
+    private void OnWinterModeButtonPressed(bool isChecked)
+    {
+        GameManager.Instance.IsWinterModeEnabled = isChecked;
+        if (GameManager.Instance.IsWinterModeEnabled)
+        {
+            GD.Print("Winter mode is enabled");
+        }
+        else
+        {
+            GD.Print("Winter mode is disabled");
+        }
+    }
 
-	private void OnNewGameButtonPressed()
-	{
-		GetTree().ChangeSceneToFile("res://UI/class_selection.tscn");
-	}
-	private void OnExitButtonPressed()
-	{
-		GetTree().Quit();
-	}
+    private void OnNewGameButtonPressed()
+    {
+        GetTree().ChangeSceneToFile("res://UI/hub.tscn");
+    }
+
+    private void OnExitButtonPressed()
+    {
+        GetTree().Quit();
+    }
 }
